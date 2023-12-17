e97ff52883aae84fca61215126fa80a393cb1230
wolandark <isitjuicyisitfresh@tutanota.com>
2023-12-17 06:34:55 +0330

    failed climber second room

---
+++ tiled files/Complete tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Complete tileset" tilewidth="8" tileheight="8" tilecount="720" columns="30">
+ <image source="../../../Gb Studio Assests/Assets/GB Assets/Tileset/Complete tileset.png" width="240" height="192"/>
+</tileset>
---
+++ tiled files/tsetLava.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tsetLava" tilewidth="8" tileheight="8" tilecount="64" columns="8">
+ <image source="../../../Gb Studio Assests/Assets/GB Assets/Tileset/gbTilesets/gbTilesets/tsetLava.png" width="64" height="64"/>
+</tileset>
