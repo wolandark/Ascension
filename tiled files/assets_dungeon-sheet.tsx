96a63bf29ef7576c66f98f207fc1f4c43c9d05b7
wolandark <isitjuicyisitfresh@tutanota.com>
2023-12-18 05:55:39 +0330

    lvl 10

---
+++ tiled files/assets_dungeon-sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="assets_dungeon-sheet" tilewidth="8" tileheight="8" tilecount="192" columns="12">
+ <image source="../../../Gb Studio Assests/Assets/GB Assets/Tileset/Dungeon Tile Set/assets_dungeon-sheet.png" width="96" height="128"/>
+</tileset>
